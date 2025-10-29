2efeaa438069bfb66ff91b338f5417ffbdb7dbdd
Monteiro <guilhermemr2011j3@gmail.com>
2025-10-28 21:48:48 -0300

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -33,7 +33,7 @@ export default function RootLayout({children,}: Readonly<{ children: React.React
         playsInline
         className="fixed top-0 left-0 w-full h-full object-cover object-bottom -z-10"
       >
-        <source src="/file.MOV" type="video/mp4" />
+        <source src="/file.mov" type="video/mp4" />
         </video>
 
 
