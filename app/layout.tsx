161e1f92a74d52dce3328225ead53426a4dad4d4
Monteiro <guilhermemr2011j3@gmail.com>
2025-10-28 22:46:51 -0300

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,14 +3,14 @@ import "./globals.css";
 import { Poppins } from "next/font/google";
 
 const poppins = Poppins({
-  subsets: ["latin"],   // charset usado
+  subsets: ["latin"], // charset usado
   weight: ["400", "500", "600", "700"], // pesos que você quer carregar
-  variable: "--font-poppins" // opcional se quiser usar em var()
+  variable: "--font-poppins", // opcional se quiser usar em var()
 });
 
 export const metadata = {
-  title: "Pré Matrícula - SEICE"
-}
+  title: "Pré Matrícula - SEICE",
+};
 // const geistSans = Geist({
 //   variable: "--font-geist-sans",
 //   subsets: ["latin"],
@@ -21,30 +21,29 @@ export const metadata = {
 //   subsets: ["latin"],
 // });
 
-export default function RootLayout({children,}: Readonly<{ children: React.ReactNode;}>) {
-
+export default function RootLayout({
+  children,
+}: Readonly<{ children: React.ReactNode }>) {
   return (
     <html className={`bg-[#141416] ${poppins.variable} font-sans`}>
       <body className="w-screen min-h-screen flex items-center overflow-x-hidden relative flex-col">
-        <video 
-        autoPlay
-        loop
-        muted
-        playsInline
-        className="fixed top-0 left-0 w-full h-full object-cover object-bottom -z-10"
-      >
-        <source src="/file.mov" type="video/mp4" />
+        <video
+          autoPlay
+          loop
+          muted
+          playsInline
+          className="fixed top-0 left-0 w-full h-full object-cover object-bottom -z-10"
+        >
+          <source src="/file.mov" type="video/mp4" />
         </video>
 
-
-      <div className="fixed top-0 left-0 w-full h-full bg-black opacity-55 z-[-5] "></div>
+        <div className="fixed top-0 left-0 w-full h-full bg-black opacity-55 z-[-5] "></div>
 
         <Account />
 
         <div className="my-auto py-2 h-full flex justify-center items-center w-full overflow-y-auto">
           {children}
         </div>
-      
       </body>
     </html>
   );
